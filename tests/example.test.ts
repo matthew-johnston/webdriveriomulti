bfdc984a2e3505654c4b8d62b0f7a681b44ed744
Matt Johnston <matcric@gmail.com>
2019-10-13 14:57:27 +1300

    Rearranged the test

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -5,13 +5,11 @@ describe("Example multiremote test", () => {
       const apiUrl = `https://webdriver.io/docs/api.html`;
       primaryBrowser.url(apiUrl);
       secondaryBrowser.url(apiUrl);
-
       const page1 = new ApiPage(primaryBrowser);
-      page1.browserGroup.selectItemByName("$");
-
       const page2 = new ApiPage(secondaryBrowser);
-      page2.browserGroup.selectItemByName("debug");
 
+      page1.browserGroup.selectItemByName("$");
+      page2.browserGroup.selectItemByName("debug");
       page1.browserGroup.selectItemByName("execute");
       page2.browserGroup.selectItemByName("call");
       page1.browserGroup.selectItemByName("pause");
